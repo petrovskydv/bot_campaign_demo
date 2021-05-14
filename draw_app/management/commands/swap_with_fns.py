dbea3cfdaf3bc458d275b43511691030e5e19a8d
Julia Kendo <ya1814@yandex.ru>
2021-05-15 01:15:47 +0300

    refactor swap with fns

--- draw_app/management/commands/swap_with_fns.py
+++ draw_app/management/commands/swap_with_fns.py
@@ -9,12 +9,8 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 from draw_app.models import FnsOrder
-from fns_open_api.bill_check import fetch_bill_check_info
+from fns_open_api.bill_check import fetch_bill_check
 from draw_app.notify_rollbar import notify_rollbar
-
-
-class SwapFinish(Exception):
-    pass
 
 
 class Command(BaseCommand):
@@ -34,7 +30,7 @@ class Command(BaseCommand):
             return super().execute(*args, **options)
 
 
-def handle_errors():
+def handle_network_errors():
     def wrap(func):
         @functools.wraps(func)
         async def run_func(queue):
@@ -47,10 +43,6 @@ def handle_errors():
                 except requests.ConnectionError:
                     await asyncio.sleep(60)
                     continue
-                except SwapFinish as message:
-                    # временная заглушка, пока не добавлена логика работы с успешно полученным чеком из фнс
-                    print(message)
-                    break
         return run_func
     return wrap
 
@@ -78,12 +70,17 @@ def fetch_raw_fns_orders():
     ]
 
 
+@sync_to_async
+def get_сheck_ticket(сheck_ticket_info, *args):
+    return fetch_bill_check(сheck_ticket_info, *args)
+
+
 async def update_tax_request(request_queue, raw_fns_order):
     request_queue.put_nowait(raw_fns_order)
     await request_queue.join()
 
 
-@handle_errors()
+@handle_network_errors()
 async def update_tax_requests(request_queue):
 
     raw_fns_orders = await fetch_raw_fns_orders()
@@ -94,16 +91,16 @@ async def update_tax_requests(request_queue):
             )
 
 
-@handle_errors()
+@handle_network_errors()
 async def fetch_tax_data(request_queue):
 
     tax_request = await request_queue.get()
-    if tax_request.qr_recognized:
-        bill_check_info = fetch_bill_check_info(
-            tax_request.qr_recognized, settings.INN,
+    if tax_request.сheck_ticket_info:
+        bill_check = await get_сheck_ticket(
+            tax_request.сheck_ticket_info, settings.INN,
             settings.CLIENT_SECRET, settings.PASSWORD
         )
-        tax_request.answer = bill_check_info
+        tax_request.answer = bill_check
         async with open_cancel_scope(shield=True):
             await sync_to_async(tax_request.save)()
 
@@ -116,16 +113,15 @@ async def update_tax_request_test(request_queue, qrcode):
     await request_queue.join()
 
 
-@handle_errors()
 async def fetch_tax_data_test(request_queue):
 
     qrcode = await request_queue.get()
 
     if qrcode:
-        bill_check_info = fetch_bill_check_info(
+        bill_check = await get_сheck_ticket(
             qrcode, settings.INN,
             settings.CLIENT_SECRET, settings.PASSWORD
         )
 
     request_queue.task_done()
-    raise SwapFinish(bill_check_info)
+    print(bill_check)
