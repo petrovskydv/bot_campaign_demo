eee6dd34febe6e980494dd4445ff10b690780c13
Julia Kendo <ya1814@yandex.ru>
2021-05-17 18:41:49 +0300

    add parsing time

--- draw_app/views.py
+++ draw_app/views.py
@@ -1,5 +1,6 @@
 import base64
 import requests
+import time
 from django.conf import settings
 from dbr import (BarcodeReader, BarcodeReaderError)
 from requests.models import HTTPError
@@ -26,29 +27,40 @@ def read_barcode(image):
     return decode_file_stream(reader, image)
 
 
-def send_to_front(chat_id, message):
+def send_to_front(chat_id, message, start_time=None):
     url = urljoin(settings.NODE_API_ENDPOINT, 'send')
+
+    text_message = str.replace(''.join(message), '&', '\n')
+
+    if start_time:
+        spend_time = '%2.2f ms' % ((time.time() - start_time) * 1000)
+        text_message = f'{text_message}\nвремя распознования: {spend_time}'
 
     query = {
         'chat_id': chat_id,
-        'text': str.replace(''.join(message), '&', '\n'),
+        'text': text_message,
     }
 
     response = requests.get(url, params=urlencode(query))
     response.raise_for_status()
-
-    return response.json()
 
 
 @api_view(['POST'])
 def handle_receipt_image(request):
     try:
         with notify_rollbar():
+            start_time = time.time()
             chat_id = request.data['chatId']
             image = base64.b64decode(request.data['content'])
             barcode = read_barcode(image)
             if barcode:
-                send_to_front(chat_id, barcode)
+                send_to_front(chat_id, barcode, start_time)
+            else:
+                send_to_front(
+                    request.data['chatId'],
+                    'Изображение не распознано!'
+                )
+
     except HTTPError as error:
         return Response(f'fail: {error}')
     except BarcodeReaderError as error:
