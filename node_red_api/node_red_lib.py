9d89dd40385bdd986819ae1ee4e35fdc9f48053d
Julia Kendo <ya1814@yandex.ru>
2021-05-31 23:49:51 +0300

    refact send messages to front

--- node_red_api/node_red_lib.py
+++ node_red_api/node_red_lib.py
@@ -1,32 +1,27 @@
 import requests
-
 from django.conf import settings
-from urllib.parse import urljoin, urlencode
+from urllib.parse import urljoin
 
 
-def send_message_to_nr(chat_id, message):
-    url = urljoin(settings.NODE_API_ENDPOINT, 'send')
+def send_message_to_nr(chat_id, text_message):
 
-    text_message = str.replace(''.join(message), '&', '\n')
-
-    query = {
-        'chat_id': chat_id,
-        'text': text_message,
-    }
-
-    response = requests.get(url, params=urlencode(query))
+    response = requests.get(
+        urljoin(settings.NODE_API_ENDPOINT, 'send'),
+        params={
+            'chat_id': chat_id,
+            'text': text_message.replace('&', ' '),
+        }
+    )
     response.raise_for_status()
 
 
-def send_message_to_tg(chat_id, message):
-    url = f'https://api.telegram.org/bot{settings.TG_TOKEN}/sendMessage'
+def send_message_to_tg(chat_id, text_message):
 
-    text_message = str.replace(''.join(message), '&', '\n')
-
-    query = {
-        'chat_id': chat_id,
-        'text': text_message,
-    }
-
-    response = requests.get(url, params=urlencode(query))
+    response = requests.get(
+        f'https://api.telegram.org/bot{settings.TG_TOKEN}/sendMessage',
+        params={
+            'chat_id': chat_id,
+            'text': text_message,
+        }
+    )
     response.raise_for_status()
